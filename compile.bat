1988922ab527a1499329e504fce3f15918ee982d
dj.tuBIG/MaliceX <djtubig.malicex@gmail.com>
2023-10-03 00:57:34 +1100

    Migrate to AudioWorklet part 1

--- compile.bat
+++ compile.bat
@@ -1,3 +1,3 @@
 @echo off
 :: requires Emscripten emsdk with emcc already activated
-emcc extern\ESFMu\esfm.c extern\ESFMu\esfm_registers.c esfmu_helper.c natv.c emscripten_wrapper.c -std=c11 -s EXPORTED_RUNTIME_METHODS=ccall,cwrap   -s EXPORTED_FUNCTIONS="['_Initialize', '_Reset', '_WriteMidiData', '_GetSample', '_GetSampleTwo', '_PlaySysex', '_CloseInstance', '_ReloadBank', '_malloc']" --embed-file banks
+emcc extern\ESFMu\esfm.c extern\ESFMu\esfm_registers.c esfmu_helper.c natv.c emscripten_wrapper.c -std=gnu11 -s EXPORTED_RUNTIME_METHODS=ccall,cwrap   -s EXPORTED_FUNCTIONS="['_Initialize', '_Reset', '_WriteMidiData', '_GetSample', '_PlaySysex', '_CloseInstance', '_ReloadBank', '_malloc']" -sAUDIO_WORKLET=1 -sWASM_WORKERS=1  --embed-file banks
--- emscripten_wrapper.c
+++ emscripten_wrapper.c
@@ -1,13 +1,89 @@
 #include <emscripten.h>
+#include <emscripten/webaudio.h>
+#include <emscripten/em_math.h>
 #include "esfmu_helper.h"
 #include "natv.h"
 
 
+static uint8_t audioThreadStack[16384];
+static EMSCRIPTEN_WEBAUDIO_T context;
+static bool isAudioStarted = false;
+
+EM_BOOL OnCanvasClick(int eventType, const EmscriptenMouseEvent *mouseEvent, void *userData)
+{
+  EMSCRIPTEN_WEBAUDIO_T audioContext = (EMSCRIPTEN_WEBAUDIO_T)userData;
+  if (emscripten_audio_context_state(audioContext) != AUDIO_CONTEXT_STATE_RUNNING) {
+    emscripten_resume_audio_context_sync(audioContext);
+  }
+  return EM_FALSE;
+}
+
+void GetSample(short *samplem, int len)
+{
+   Render(samplem, len);
+}
+
+EM_BOOL GenerateRender(int numInputs, const AudioSampleFrame *inputs,
+                       int numOutputs, AudioSampleFrame *outputs,
+                       int numParams, const AudioParamFrame *params,
+                       void *userData)
+{
+   static short buf[256];
+
+   GetSample(buf, 256);
+   for(int i = 0; i < 128*outputs[0].numberOfChannels; i+=2)
+   {
+      outputs[0].data[i] = buf[i];
+   }
+
+  return EM_TRUE; // Keep the graph output going
+}
+
+void AudioWorkletProcessorCreated(EMSCRIPTEN_WEBAUDIO_T audioContext, EM_BOOL success, void *userData)
+{
+  if (!success) return; // Check browser console in a debug build for detailed errors
+
+  int outputChannelCounts[2] = { 1, 2 };
+  EmscriptenAudioWorkletNodeCreateOptions options = {
+    .numberOfInputs = 0,
+    .numberOfOutputs = 1,
+    .outputChannelCounts = outputChannelCounts
+  };
+
+  // Create node
+  EMSCRIPTEN_AUDIO_WORKLET_NODE_T wasmAudioWorklet = emscripten_create_wasm_audio_worklet_node(audioContext,
+                                                            "esfm-generator", &options, &GenerateRender, 0);
+
+  // Connect it to audio context destination
+  EM_ASM({emscriptenGetAudioObject($0).connect(emscriptenGetAudioObject($1).destination)},
+    wasmAudioWorklet, audioContext);
+
+  // Resume context on mouse click
+  emscripten_set_click_callback("body", (void*)audioContext, 0, OnCanvasClick);
+}
+
+void AudioThreadInitialized(EMSCRIPTEN_WEBAUDIO_T audioContext, EM_BOOL success, void *userData)
+{
+   printf("AudioThread Debug init\n");
+  if (!success) return; // Check browser console in a debug build for detailed errors
+  WebAudioWorkletProcessorCreateOptions opts = {
+    .name = "esfm-generator",
+  };
+  emscripten_create_wasm_audio_worklet_processor_async(audioContext, &opts, &AudioWorkletProcessorCreated, 0);
+}
+
+
 void Initialize() 
 {
-   InitWaveOut();
-   StartWaveOut();
-
+   if (!isAudioStarted) {
+      InitWaveOut();
+      StartWaveOut();
+      context = emscripten_create_audio_context(0);
+      emscripten_start_wasm_audio_worklet_thread_async(context, audioThreadStack, sizeof(audioThreadStack),
+                                                   &AudioThreadInitialized, 0);
+      isAudioStarted = true;
+   }
+   
    LoadBank("banks/bnk_common.bin");
    fmreset();
 }
@@ -19,30 +95,7 @@ void Reset()
 
 void WriteMidiData(uint32_t dwData)
 {
-   // if (instance != nullptr)
-   // {
-   //    //std::cout << "WriteMidiData(" << dwData << ")" << std::endl;
-   //    instance->WriteMidiData(dwData);
-   // }
-   //printf("Debug MIDI Test: %x\n", dwData);
    MidiMessage(dwData);
-}
-
-void GetSample(short *samplem, int len)
-{
-   Render(samplem, len);
-}
-
-void GetSampleTwo(short *samplem, int len, float* fsamparrL, float* fsamparrR)
-{
-   GetSample(samplem, len);
-   
-   for (int i = 0, j = 0; i < len; )
-   {
-      fsamparrL[j]   = samplem[i++] / 32768.f;
-      fsamparrR[j++] = samplem[i++] / 32768.f;
-   }
-   
 }
 
 void PlaySysex(uint8_t *bufpos, uint32_t len)
